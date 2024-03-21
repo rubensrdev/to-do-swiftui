2968749fdb5aabe0c51ea45a6ce1328a5d308213
Rubén <ruben.sr.apple@gmail.com>
2024-03-21 10:43:05 +0100

    creación del proyecto en xcode

---
+++ to-do/to-do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to-do
+//
+//  Created by Ruben on 21/3/24.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
